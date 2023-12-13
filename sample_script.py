61ba2761bd01fb4529ef1c2dbb6cd9e1c421e7ed
Max Reid <max.reid11@gmail.com>
2023-12-13 14:19:22 -0800

    This is a test

--- sample_script.py
+++ sample_script.py
@@ -1,15 +1,37 @@
 import requests, json
 
-def fetchData(url):
-  while True:  
-    try:
-      r=requests.get(url)
-      if r.status_code==200:
-          data=r.json()
-          return data
-      else: print('Error:', r.status_code)
-    except Exception as e: print(e)
+def fetch_data(url):
+    ""
+    Fetch data from the specified URL.
 
-url='https://api.example.com/data'
-data=fetchData(url)
-if data!=None: print(data)
+    Attempts to retrieve JSON sata from the URL. If the request is successful (status code 200),
+    returns the JSON response. In case of failure, prints the error and returns None.
+
+    Args:
+        url (str): The URL to fetch data from.
+    Returns: 
+        dict or None: The JSON response if successful, otherwise None.
+    """
+    retry_limit = 3
+    retries = 0
+
+    while retries < retry_limit:
+        try:
+            r = requests.get(url)
+          if r.status_code == 200:
+              return r.json()
+            else:
+                print(f'Error: {r.status_code}')
+                retries += 1
+        except Exception as e:
+            print(f'Exception occurred: {e}')
+            retries += 1
+
+    return None
+
+# URL to fetch data from
+url = 'https://api.example.com/data'
+data = fetch_data(url)
+
+if data is not None:
+    print(data)
