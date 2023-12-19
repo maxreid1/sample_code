ec4a57cd2bb6d095c36a6a0ab7536262f7d1f5cc
Max Reid <max.reid11@gmail.com>
2023-12-19 08:13:18 -0800

    fixing for standards

--- sample_script.py
+++ sample_script.py
@@ -1,15 +1,18 @@
-import requests, json
+#import os
 
-def fetchData(url):
-  while True:  
-    try:
-      r=requests.get(url)
-      if r.status_code==200:
-          data=r.json()
-          return data
-      else: print('Error:', r.status_code)
-    except Exception as e: print(e)
+def write_file():
+    # Create a new file and write some text to it
+    with open('languages.txt', 'w') as file_handler:
+        file_handler.write("Bash\\n")
+        file_handler.write("Python\\n")
+        file_handler.write("PHP\n")
 
-url='https://api.example.com/data'
-data=fetchData(url)
-if data!=None: print(data)
+def read_file():
+    # Open the file and read its contents
+    with open('languages.txt', 'r') as file_handler:
+        for line in file_handler:
+            print(line)
+
+# Write to and read from the file
+_write_file()
+read_file()
